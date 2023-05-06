be3ed409bfb8b5936327eda3011fcdf90b98062a
Jaydip <jaydipshinga12@gmail.com>
2023-05-06 15:53:39 +0530

    Commit Message

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:jaydip_flutter/widgets/BottomAppBar.dart';
 import 'package:jaydip_flutter/widgets/DraggableScrollableSheet.dart';
+import 'package:jaydip_flutter/widgets/LowLevelAnimation.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomappbarApp(),
+      home: AnimationinnormalwgApp(),
     ),
   );
 }
---
+++ lib/widgets/LowLevelAnimation.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class AnimationinnormalwgApp extends StatefulWidget {
+  const AnimationinnormalwgApp({Key? key}) : super(key: key);
+
+  @override
+  State<AnimationinnormalwgApp> createState() => _AnimationinnormalwgAppState();
+}
+
+class _AnimationinnormalwgAppState extends State<AnimationinnormalwgApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController? maincontroller;
+  CurvedAnimation? designanimation;
+  Animation<double>? sizeanimation;
+  Animation<Color?>? coloranimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    maincontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    designanimation =
+        CurvedAnimation(parent: maincontroller!, curve: Curves.bounceInOut);
+    sizeanimation =
+        Tween<double>(begin: 100, end: 200).animate(designanimation!);
+    coloranimation = ColorTween(begin: Colors.amberAccent, end: Colors.deepOrange)
+        .animate(designanimation!);
+
+    maincontroller!.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation in Normal Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: FlutterLogo(),
+              height: sizeanimation!.value,
+              width: sizeanimation!.value,
+              color: coloranimation!.value,
+              padding: EdgeInsets.all(20),
+            ),
+            ElevatedButton(
+              onPressed: maincontroller!.isCompleted
+                  ? null
+                  : () => maincontroller!.forward(),
+              child: Text('Forward'),
+            ),
+            ElevatedButton(
+              onPressed: maincontroller!.isDismissed
+                  ? null
+                  : () => maincontroller!.reverse(),
+              child: Text('Reverse'),
+            ),
+            // ElevatedButton(
+            //   onPressed: maincontroller!.isCompleted
+            //       ? null
+            //       : () => maincontroller!.forward(),
+            //   child: Text('Play'),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
