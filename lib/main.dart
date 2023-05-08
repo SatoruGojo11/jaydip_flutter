9d362dac152934f054eaf23a0fb9c22f2d272a22
Jaydip <jaydipshinga12@gmail.com>
2023-05-08 17:05:00 +0530

    Animated Widget Update 08/05/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:jaydip_flutter/widgets/AnimatedWidgetApp.dart';
 import 'package:jaydip_flutter/widgets/SliverappbarApp.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SliverappbarApp(),
+      home: MakeownAnimatedWidgetApp(),
     ),
   );
 }
-
-
---
+++ lib/widgets/AnimatedWidgetApp.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class MakeownAnimatedWidgetApp extends StatefulWidget {
+  const MakeownAnimatedWidgetApp({Key? key}) : super(key: key);
+
+  @override
+  State<MakeownAnimatedWidgetApp> createState() =>
+      _MakeownAnimatedWidgetAppState();
+}
+
+class _MakeownAnimatedWidgetAppState extends State<MakeownAnimatedWidgetApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController? Mycontroller;
+  Animation<double>? sizeAnimation;
+  Animation<Color?>? colorAnimation;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    Mycontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 4));
+    sizeAnimation = Tween<double>(begin: 100, end: 300).animate(Mycontroller!);
+    colorAnimation =
+        ColorTween(begin: Colors.deepPurpleAccent, end: Colors.indigoAccent)
+            .animate(Mycontroller!);
+
+    Mycontroller!.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Animated Widget'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _AnimatedContainer(
+              Animatedcolor: colorAnimation,
+              Animatedsize: sizeAnimation,
+            ),
+            ElevatedButton(
+                onPressed: Mycontroller!.isCompleted
+                    ? null
+                    : () => Mycontroller!.forward(),
+                child: Text('Forward Animation')),
+            ElevatedButton(
+                onPressed: Mycontroller!.isDismissed
+                    ? null
+                    : () => Mycontroller!.reverse(),
+                child: Text('Reverse Animation')),
+            ElevatedButton(
+                onPressed: () => Mycontroller!.repeat(reverse: true),
+                child: Text('Loop Animation')),
+            ElevatedButton(
+                onPressed: () => Mycontroller!.stop(),
+                child: Text('Reverse Animation')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AnimatedContainer extends AnimatedWidget {
+  Animation<double>? Animatedsize;
+  Animation<Color?>? Animatedcolor;
+
+  _AnimatedContainer({this.Animatedsize, this.Animatedcolor})
+      : super(listenable: Animatedsize!);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> sizeAnimation = listenable as Animation<double>;
+
+    return Container(
+      child: FlutterLogo(),
+      height: Animatedsize!.value,
+      width: Animatedsize!.value,
+      color: Animatedcolor!.value,
+      padding: EdgeInsets.all(20),
+    );
+  }
+}
