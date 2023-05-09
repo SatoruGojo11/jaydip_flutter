4f8dfddfa923861b97fb7f1ee21012c457ffbf39
Jaydip <jaydipshinga12@gmail.com>
2023-05-09 21:38:22 +0530

    Lootie App 09/05/2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:jaydip_flutter/widgets/AnimatedWidgetApp.dart';
-import 'package:jaydip_flutter/widgets/SliverappbarApp.dart';
+import 'package:jaydip_flutter/widgets/LottieUseApp.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MakeownAnimatedWidgetApp(),
+      home: LottieUseApp(),
     ),
   );
 }
---
+++ lib/widgets/LottieUseApp.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class LottieUseApp extends StatefulWidget {
+  const LottieUseApp({Key? key}) : super(key: key);
+
+  @override
+  State<LottieUseApp> createState() => _LottieUseAppState();
+}
+
+class _LottieUseAppState extends State<LottieUseApp>
+    with SingleTickerProviderStateMixin {
+  AnimationController? Ltcontroller;
+  bool play = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Ltcontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lootie App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 500,
+              width: 600,
+              child: Lottie.network(
+                  'https://assets8.lottiefiles.com/packages/lf20_poqmycwy.json',
+                  controller: Ltcontroller,
+                  fit: BoxFit.fill),
+            ),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () => Ltcontroller!.forward(),
+                    icon: Icon(Icons.skip_next)),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        play = !play;
+                        Ltcontroller!.isAnimating
+                            ? () => Ltcontroller!.stop()
+                            : () => Ltcontroller!.repeat(reverse: true);
+                      });
+                    },
+                    icon: Icon(play ? Icons.play_arrow : Icons.pause)),
+                IconButton(
+                    onPressed: () => Ltcontroller!.reverse(),
+                    icon: Icon(Icons.skip_previous)),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
